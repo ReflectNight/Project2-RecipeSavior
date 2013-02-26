9f7b23a7977f6cabd51910c5ee76704af8f7ace6
Angela Qiu <aqiu@andrew.cmu.edu>
2013-02-25 21:32:41 -0500

    canvas added things

--- static/canvasfn.js
+++ static/canvasfn.js
@@ -1,7 +1,9 @@
 //All canvas functions taken care of here
 
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+redrawCanvas();
 
 
 function onMouseDown(event) {
@@ -11,4 +13,36 @@ function onMouseDown(event) {
     //figure out the area of all the boxes and arrows
 
 }
+
+function redrawCanvas(){
+	console.log("redrawing...");
+	clearCanvas();
+	ctx.fillStyle = "rgba(254, 251 ,252, 0.25)";
+	roundedRect(ctx, 40, 125, 400, 500, 20);
+	ctx.fillRect(40, 125, 400, 500);
+
+}
+
+function roundedRect(ctx,x,y,width,height,radius){
+    ctx.beginPath();
+    ctx.moveTo(x,y+radius);
+    ctx.lineTo(x,y+height-radius);
+    ctx.quadraticCurveTo(x,y+height,x+radius,y+height);
+    ctx.lineTo(x+width-radius,y+height);
+    ctx.quadraticCurveTo(x+width,y+height,x+width,y+height-radius);
+    ctx.lineTo(x+width,y+radius);
+    ctx.quadraticCurveTo(x+width,y,x+width-radius,y);
+    ctx.lineTo(x+radius,y);
+    ctx.quadraticCurveTo(x,y,x,y+radius);
+    ctx.stroke();
+}
+
+
 canvas.addEventListener('mousedown', onMouseDown, false);
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var w = canvas.width;
+  canvas.width = 1;
+  canvas.width = w;
+}
